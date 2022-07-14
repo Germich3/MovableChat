5e6becc693abbc851f9cc0e4c62282b94b3b86cc
Germich3 <germich3@gmail.com>
2022-07-14 16:02:44 +0200

    log update

--- src/main/java/es/germich3/movablechat/MovableChat.java
+++ src/main/java/es/germich3/movablechat/MovableChat.java
@@ -18,10 +18,7 @@ public class MovableChat implements ModInitializer {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
-
-		LOGGER.info("Hello Fabric world!");
-
 		AutoConfig.register(MovableChatConfig.class, JanksonConfigSerializer::new);
-
+		LOGGER.info("MovableChat loaded successfully");
 	}
 }
