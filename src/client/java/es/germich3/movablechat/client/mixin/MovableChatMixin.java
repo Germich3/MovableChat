f9d1ca166f7dbd7b0d6f6d3310d2be83eccb64dc
Germich3 <germich3@gmail.com>
2026-06-16 21:40:15 +0200

    add other mixins injectors to test

--- src/client/java/es/germich3/movablechat/client/mixin/MovableChatMixin.java
+++ src/client/java/es/germich3/movablechat/client/mixin/MovableChatMixin.java
@@ -1,5 +1,6 @@
 package es.germich3.movablechat.client.mixin;
 
+import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
 import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
 import es.germich3.movablechat.config.MovableChatConfig;
@@ -9,7 +10,10 @@ import org.joml.Matrix3x2f;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.*;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.invoke.arg.Args;
 
 import java.util.function.Consumer;
 
@@ -20,6 +24,22 @@ public abstract class MovableChatMixin {
 	@Final
 	private Minecraft minecraft;
 
+	@Unique
+	private static boolean enabled(String injector) {
+		return switch (injector) {
+			case "modifyConstant" -> false;
+			case "modifyExpressionValue" -> false;
+			case "modifyVariable" -> false;
+			case "inject" -> false;
+			case "modifyArg" -> false;
+			case "modifyArgs" -> false;
+			case "wrapOperation" -> true;
+			case "redirect" -> false;
+			default -> false;
+		};
+	}
+
+	@Unique
 	private int getOffset() {
 		if (MovableChatConfig.getConfig().isAbsorptionAutoMoveEnabled()) {
 			int offset = 0;
@@ -42,19 +62,133 @@ public abstract class MovableChatMixin {
 		}
 	}
 
+	@ModifyConstant(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			constant = @Constant(intValue = 40, ordinal = 0)
+	)
+	private int modifyContantFirst40(int original) {
+		if (!enabled("modifyConstant")) {
+			return original;
+		}
+		return original + getOffset();
+	}
+
+	@ModifyExpressionValue(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/util/Mth;floor(F)I"
+			)
+	)
+	private int modifyMthFloor(int original) {
+		if (!enabled("modifyExpressionValue")) {
+			return original;
+		}
+		return original - getOffset();
+	}
+
+	@ModifyVariable(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			at = @At("STORE"),
+			name = "chatBottom"
+	)
+	private int modifyChatBottom(int value) {
+		if (!enabled("modifyVariable")) {
+			return value;
+		}
+		return value - getOffset();
+	}
+
+	@Inject(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			at = @At(
+					value = "INVOKE", // <- can be HEAD/RETURN/INVOKE/TAIL
+					target = "Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;updatePose(Ljava/util/function/Consumer;)V",
+					shift = At.Shift.AFTER
+			)
+	)
+	private void injectOnExtractRenderStateAfterUpdatePose(ChatComponent.ChatGraphicsAccess graphics, int i, int j, ChatComponent.DisplayMode displayMode, CallbackInfo ci) {
+		if (!enabled("inject")) {
+			return;
+		}
+		graphics.updatePose((pose) -> {
+			pose.translate(0.0F, -getOffset());
+		});
+	}
+
+	@ModifyArg(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			index = 0,
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;updatePose(Ljava/util/function/Consumer;)V"
+			)
+	)
+	private Consumer<Matrix3x2f> modifyConsumerUpdatePose(Consumer<Matrix3x2f> originalConsumer) {
+		if (!enabled("modifyArg")) {
+			return originalConsumer;
+		}
+		return (pose) -> {
+			originalConsumer.accept(pose);
+			pose.translate(0.0F, -getOffset());
+		};
+	}
+
+	@ModifyArgs(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;updatePose(Ljava/util/function/Consumer;)V"
+			)
+	)
+	private void modifyUpdatePose(Args originalArgs) {
+		if (!enabled("modifyArgs")) {
+			return;
+		}
+		Consumer<Matrix3x2f> arg0 = originalArgs.get(0);
+		Consumer<Matrix3x2f> modifiedArg0 = pose -> {
+			arg0.accept(pose);
+			pose.translate(0.0F, -getOffset());
+		};
+		originalArgs.setAll(modifiedArg0);
+	}
+
 	@WrapOperation(
-		method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;updatePose(Ljava/util/function/Consumer;)V"
-		)
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;updatePose(Ljava/util/function/Consumer;)V"
+			)
 	)
 	private void wrapUpdatePose(ChatComponent.ChatGraphicsAccess graphics, Consumer<Matrix3x2f> originalConsumer, Operation<Void> original) {
+		if (!enabled("wrapOperation")) {
+			original.call(graphics, originalConsumer);
+			return;
+		}
 		Consumer<Matrix3x2f> modified = pose -> {
 			originalConsumer.accept(pose);
 			pose.translate(0.0F, -getOffset());
 		};
 		original.call(graphics, modified);
+	}
+
+	@Redirect(
+			method = "extractRenderState(Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;IILnet/minecraft/client/gui/components/ChatComponent$DisplayMode;)V",
+			at = @At(
+					value = "INVOKE",
+					target = "Lnet/minecraft/client/gui/components/ChatComponent$ChatGraphicsAccess;updatePose(Ljava/util/function/Consumer;)V",
+					ordinal = 0
+			)
+	)
+	private void redirectUpdatePose(ChatComponent.ChatGraphicsAccess graphics, Consumer<Matrix3x2f> originalConsumer) {
+		if (!enabled("redirect")) {
+			graphics.updatePose(originalConsumer);
+			return;
+		}
+		graphics.updatePose((pose) -> {
+			originalConsumer.accept(pose);
+			pose.translate(0.0F, -getOffset());
+		});
 	}
 
 }
